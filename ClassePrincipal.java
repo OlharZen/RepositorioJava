9d2caef7b83e801243dae56ac8cc94c9b2ce8eb5
lab04 <lab04@lab4-08>
2024-04-25 22:32:01 -0300

    incluido if else

--- ClassePrincipal.java
+++ ClassePrincipal.java
@@ -2,7 +2,14 @@
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("Hello, World!!");
+		
+		int idade  = 18;
+		if(idade > 18) {
+			System.out.printlm("Okay");
+			
+		}
 
 	}
 
